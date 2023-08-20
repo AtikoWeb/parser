fd995919021fc6b214db438a39c5602f4cfc8c92
AtikoWeb <dev.atikoweb@gmail.com>
2023-08-20 11:29:15 +0600

    Update parser.js

--- parser.js
+++ parser.js
@@ -3,7 +3,6 @@ import fs from 'fs/promises';
 
 export async function parser(email, password, fileName) {
 	try {
-		// Начало парсинга
 		console.time('Parser');
 
 		const browser = await puppeteer.launch({
@@ -13,85 +12,68 @@ export async function parser(email, password, fileName) {
 		});
 
 		const page = await browser.newPage();
-		await page.setViewport({ width: 1920, height: 1080 });
 
 		await page.goto('https://kaspi.kz/mc/#/login', {
-			timeout: 60000, // Поправка: увеличить время ожидания загрузки страницы до 60 секунд
+			timeout: 60000,
 		});
+
+		await page.screenshot({ path: 'image.png' });
 
 		const navbar = await page.$('.navbar-item');
 
-		// Вход в Кабинет
 		if (!navbar) {
 			await page.waitForSelector('.tabs.is-centered.is-fullwidth');
 			await page.click('.tabs.is-centered.is-fullwidth li:nth-child(2)');
-
 			await page.waitForSelector('#user_email');
-
 			await page.type('#user_email', email);
-
+			await page.screenshot({ path: 'image.png' });
 			const buttonContinue = await page.waitForSelector(
 				'button[class="button is-primary"]:not(:empty):not(:has(*))'
 			);
 			await buttonContinue.click();
-
 			await page.waitForSelector('input[type="password"]');
-
 			await page.type('input[type="password"]', password);
-
+			await page.screenshot({ path: 'image.png' });
 			const buttonSubmit = await page.waitForSelector(
 				'button[class="button is-primary"]:not(:empty):not(:has(*))'
 			);
 			await buttonSubmit.click();
-
+			await page.screenshot({ path: 'image.png' });
 			await page.waitForSelector('.navbar-item');
 		}
 
-		await page.goto(`https://kaspi.kz/mc/#/products/ACTIVE/1`, {
-			timeout: 60000, // Поправка: увеличить время ожидания загрузки страницы до 60 секунд
+		await page.goto('https://kaspi.kz/mc/#/products/ACTIVE/1', {
+			timeout: 120000,
 		});
-
-		await page.waitForNavigation();
 		await page.screenshot({ path: 'image.png' });
 
 		let isButtonEnabled = true;
-
 		const products = [];
 		const ids = new Set();
 
 		while (isButtonEnabled) {
 			const productRows = await page.$$('tbody tr');
-			const numRows = productRows.length < 10 ? productRows.length : 10;
+			const numRows = Math.min(productRows.length, 10);
 
 			for (let i = 0; i < numRows; i++) {
 				const productRow = productRows[i];
-
 				const productNameElement = await productRow.$(
 					'td[data-label="Товар"] a'
 				);
-				const productName = await (
-					await productNameElement.getProperty('textContent')
-				).jsonValue();
-
-				const productUrl = await (
-					await productNameElement.getProperty('href')
-				).jsonValue();
-
+				const productName = await productNameElement.evaluate(
+					(el) => el.textContent
+				);
+				const productUrl = await productNameElement.evaluate((el) => el.href);
 				const productPrice = await productRow.$(
 					'td[data-label="Цена, тенге"] p.subtitle.is-5'
 				);
-
-				const price = await page.evaluate(
-					(productPrice) => productPrice.textContent.trim(),
-					productPrice
+				const price = await productPrice.evaluate((el) =>
+					el.textContent.trim()
 				);
-
 				const idRegExp = /(\d+)\/$/;
 				const matches = productUrl.match(idRegExp);
 				const id = matches[1];
-
 				ids.add(id);
-
 				products.push({
 					id,
 					name: productName,
@@ -101,19 +83,14 @@ export async function parser(email, password, fileName) {
 			}
 
 			const nextPageButton = await page.waitForSelector('.pagination-next', {
-				timeout: 60000,
+				timeout: 120000,
 			});
 
-			const isDisabled = await page.evaluate(
-				(el) => el.hasAttribute('disabled'),
-				nextPageButton
+			const isDisabled = await nextPageButton.evaluate((el) =>
+				el.hasAttribute('disabled')
 			);
-
 			const pageInfoElement = await page.$('.page-info');
-			const pageText = await page.evaluate(
-				(el) => el.textContent,
-				pageInfoElement
-			);
+			const pageText = await pageInfoElement.evaluate((el) => el.textContent);
 			const matches = pageText.match(/из\s+(\d+)/);
 			const totalProducts = matches[1];
 
@@ -121,21 +98,17 @@ export async function parser(email, password, fileName) {
 				isButtonEnabled = false;
 			} else if (!isDisabled) {
 				await nextPageButton.click();
-				await page.waitForSelector('.pagination-next', { timeout: 60000 });
 			} else {
 				isButtonEnabled = false;
 			}
 			console.log(`Parsing... ${pageText}`);
 		}
 
-		console.timeEnd('Parser'); // Поправка: переместить console.timeEnd в конец, чтобы замерить время после завершения цикла.
-
+		console.timeEnd('Parser');
 		console.log(products.length);
 
 		const space = 2;
-
 		const name = fileName + '.json';
-
 		await fs.writeFile(name, JSON.stringify(products, null, space));
 		console.log(`Результаты сохранены в файл ${name}`);
 

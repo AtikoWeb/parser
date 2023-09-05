33e583af9ad2c96217a52e141dd8e1322d252a79
AtikoWeb <dev.atikoweb@gmail.com>
2023-09-05 19:26:28 +0600

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,24 +1,26 @@
-import { webkit } from 'playwright';
+import puppeteer from 'puppeteer';
 import fs from 'fs/promises';
 
 export async function parser(email, password, fileName) {
 	try {
 		console.time('Parser');
 
-		const browser = await webkit.launch({
-			headless: true,
+		const browser = await puppeteer.launch({
+			headless: 'new',
+			args: ['--no-sandbox', '--disable-dev-shm-usage'],
+			ignoreHTTPSErrors: true,
+			ignoreDefaultArgs: ['--disable-extensions'],
 		});
 
-		const context = await browser.newContext();
-		const page = await context.newPage();
+		const page = await browser.newPage();
 
-		await page.setViewportSize({
-			width: 1920,
-			height: 1080,
+		await page.setViewport({
+			width: 1280, // Ширина окна в пикселях
+			height: 720, // Высота окна в пикселях
 		});
 
 		await page.goto('https://kaspi.kz/mc/#/login', {
-			timeout: 90000,
+			waitUntil: 'domcontentloaded',
 		});
 
 		await page.screenshot({ path: 'image.png' });
@@ -42,18 +44,15 @@ export async function parser(email, password, fileName) {
 				'button[class="button is-primary"]:not(:empty):not(:has(*))'
 			);
 			await buttonSubmit.click();
+			await page.screenshot({ path: 'image.png' });
 			await page.waitForSelector('.navbar-item');
 		}
 
+		await page.waitForTimeout(3000);
+		await page.goto('https://kaspi.kz/mc/#/products/ACTIVE/1', {
+			waitUntil: 'domcontentloaded',
+		});
 		await page.screenshot({ path: 'image.png' });
-
-		const productsButton = await page.waitForSelector(
-			'#sidebar-offers-products'
-		);
-
-		await productsButton.click();
-		await page.screenshot({ path: 'image.png' });
-		await page.waitForSelector('.pagination-next');
 
 		let isButtonEnabled = true;
 		const products = [];
@@ -68,12 +67,16 @@ export async function parser(email, password, fileName) {
 				const productNameElement = await productRow.$(
 					'td[data-label="Товар"] a'
 				);
-				const productName = await productNameElement.textContent();
-				const productUrl = await productNameElement.getAttribute('href');
+				const productName = await productNameElement.evaluate(
+					(el) => el.textContent
+				);
+				const productUrl = await productNameElement.evaluate((el) => el.href);
 				const productPrice = await productRow.$(
 					'td[data-label="Цена, тенге"] p.subtitle.is-5'
 				);
-				const price = await productPrice.textContent();
+				const price = await productPrice.evaluate((el) =>
+					el.textContent.trim()
+				);
 				const idRegExp = /(\d+)\/$/;
 				const matches = productUrl.match(idRegExp);
 				const id = matches[1];
@@ -90,10 +93,11 @@ export async function parser(email, password, fileName) {
 				timeout: 120000,
 			});
 
-			const isDisabled =
-				(await nextPageButton.getAttribute('disabled')) !== null;
+			const isDisabled = await nextPageButton.evaluate((el) =>
+				el.hasAttribute('disabled')
+			);
 			const pageInfoElement = await page.$('.page-info');
-			const pageText = await pageInfoElement.textContent();
+			const pageText = await pageInfoElement.evaluate((el) => el.textContent);
 			const matches = pageText.match(/из\s+(\d+)/);
 			const totalProducts = matches[1];
 
@@ -101,10 +105,6 @@ export async function parser(email, password, fileName) {
 				isButtonEnabled = false;
 			} else if (!isDisabled) {
 				await nextPageButton.click();
-				await page.waitForSelector('.pagination-next', {
-					timeout: 120000,
-				});
-				await page.screenshot({ path: 'image.png' });
 			} else {
 				isButtonEnabled = false;
 			}
